9870a22a274f3d606dc3f8b8e4c5d6e4eab8f05c
Greg Allen <greg@firstandthird.com>
2017-12-16 13:30:25 -0800

    fix if not manager

--- index.js
+++ index.js
@@ -129,8 +129,8 @@ const main = async function() {
           { title: 'Availability', value: d.Spec.Availability, short: true },
           { title: 'Status', value: d.Status.State, short: true },
           { title: 'Hostname', value: d.Description.Hostname, short: true },
-          { title: 'Leader', value: d.ManagerStatus.Leader, short: true },
-          { title: 'Reachability', value: d.ManagerStatus.Reachability, short: true }
+          { title: 'Leader', value: d.ManagerStatus ? d.ManagerStatus.Leader : '', short: true },
+          { title: 'Reachability', value: d.ManagerStatus ? d.ManagerStatus.Reachability : '', short: true }
         ]
       }))
     };
